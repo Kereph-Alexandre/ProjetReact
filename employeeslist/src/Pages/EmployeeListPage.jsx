275997f6244ebe5869d0de81169dfb6b2611cc77
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-12-07 16:51:57 +0100

    feat: Ajout de la page Liste :sparkles:

---
+++ employeeslist/src/Pages/EmployeeListPage.jsx
@@ -0,0 +1,11 @@
+import EmployeeList from "../Components/EmployeeList/EmployeeList";
+
+export default function EmployeeListPage() {
+  return (
+    <>
+      <h1>Liste des Employés</h1>
+
+      <EmployeeList />
+    </>
+  );
+}
