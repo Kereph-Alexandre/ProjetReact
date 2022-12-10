e360d3473b6a85a5b74f663c46785c7ba7d0aa69
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2022-12-10 16:50:08 +0100

    feat: Ajout boutons Composant Profil :sparkles:

--- employeeslist/src/Pages/EmployeeFocusPage.jsx
+++ employeeslist/src/Pages/EmployeeFocusPage.jsx
@@ -5,10 +5,28 @@ import EmployeeData from "../Data/Mock_data.json";
 export default function EmployeeFocusPage() {
   const [employeeIndex, setEmployeeIndex] = useState(0);
 
+  function addOne() {
+    setEmployeeIndex(employeeIndex + 1);
+    if (employeeIndex >= EmployeeData.length - 1) {
+      setEmployeeIndex(0);
+    }
+  }
+
+  function substractOne() {
+    setEmployeeIndex(employeeIndex - 1);
+
+    if (employeeIndex <= 0) {
+      setEmployeeIndex(EmployeeData.length - 1);
+    }
+  }
+
   const focus = EmployeeData[employeeIndex];
+
   return (
     <>
       <EmployeeProfile focus={focus} />
+      <button onClick={addOne}>+1</button>
+      <button onClick={substractOne}>-1</button>
     </>
   );
 
